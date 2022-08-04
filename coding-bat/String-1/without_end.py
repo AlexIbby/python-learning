7da8e073f27dff03ee6579a02e1f9e7b0a0635d6
AlexIbby <alex.a.ibrahim@gmail.com>
2022-08-03 23:23:13 -0400

    Coding Bat exercises for String-1

---
+++ coding-bat/String-1/without_end.py
@@ -0,0 +1,16 @@
+# Problem
+
+
+# Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+
+# without_end('Hello') → 'ell'
+# without_end('java') → 'av'
+# without_end('coding') → 'odin'
+
+def without_end(str):
+    if len(str) > 2:
+        str = str[1:-1]
+        return str
+    if len(str) <= 2:
+        return ""
