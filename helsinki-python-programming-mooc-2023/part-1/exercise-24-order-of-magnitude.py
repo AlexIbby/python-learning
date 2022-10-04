b8ab25af67038f7893a3cdbe3b824050b1122fad
AlexIbby <alex.a.ibrahim@gmail.com>
2022-10-04 14:53:28 -0400

    completed order of magnitude exercise

---
+++ helsinki-python-programming-mooc-2023/part-1/exercise-24-order-of-magnitude.py
@@ -0,0 +1,16 @@
+# Write your solution here
+
+number = str(input("Please type in a number:"))
+number = int(number)
+
+if number < 1000:
+    print("This number is smaller than 1000")
+
+if number < 100:
+    print("This number is smaller than 100")
+
+if number <10:
+    print("This number is smaller than 10")
+
+print("Thank you!")
+
