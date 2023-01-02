2a3c1a9cdec98fd0e9b31be0fc3d9f45d0724a1e
AlexIbby <alex.a.ibrahim@gmail.com>
2023-01-01 21:02:53 -0500

    completed exercise 8, name & address

---
+++ helsinki-python-programming-mooc-2023/part-1/exercise-8-name-and-address.py
@@ -0,0 +1,11 @@
+# Write your solution here
+
+first_name = input("What is your first name")
+last_name = input("What is your last name")
+street_adress = input("What is your street address")
+city_postal_code = input("What is your city and postal code?")
+
+
+print(f"{first_name} {last_name}")
+print(street_adress)
+print(city_postal_code)
