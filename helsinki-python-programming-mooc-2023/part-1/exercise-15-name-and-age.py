479159aa236d721854e5ec5c57225c5d8b746b89
AlexIbby <alex.a.ibrahim@gmail.com>
2023-01-07 13:43:31 -0500

    completed exercise 15 - name and age

---
+++ helsinki-python-programming-mooc-2023/part-1/exercise-15-name-and-age.py
@@ -0,0 +1,7 @@
+# Write your solution here
+
+name = input("What is your name? ")
+age = input("Which year were you born? ")
+age = 2021 - int(age)
+
+print(f"Hi {name}, you will be {age}  years old at the end of the year 2021")
