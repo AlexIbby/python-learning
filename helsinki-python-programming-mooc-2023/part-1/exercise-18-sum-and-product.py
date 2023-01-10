178caa4430e57bb51d7fa6171dfdc46026bb4710
AlexIbby <alex.a.ibrahim@gmail.com>
2023-01-10 11:47:28 -0500

    completed exercise 18 - sum and product

---
+++ helsinki-python-programming-mooc-2023/part-1/exercise-18-sum-and-product.py
@@ -0,0 +1,8 @@
+number1 = input("Number 1: ")
+number2 = input("Number 2: ")
+
+number1 = int(number1)
+number2 = int(number2)
+
+print(f"The sum of the numbers: {number1 + number2}")
+print(f"The product of the numbers: {number1 * number2}")
