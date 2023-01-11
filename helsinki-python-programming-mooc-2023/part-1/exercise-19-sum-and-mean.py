b11e8e1347a51e62e696bc02e45a56b3091c3bb6
AlexIbby <alex.a.ibrahim@gmail.com>
2023-01-11 11:56:47 -0500

    completed exercise 19 - sum and mean

---
+++ helsinki-python-programming-mooc-2023/part-1/exercise-19-sum-and-mean.py
@@ -0,0 +1,11 @@
+# Write your solution here
+
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+number3 = int(input("Number 3: "))
+number4 = int(input("Number 4: "))
+
+number_sum = [number1, number2, number3, number4]
+number_sum = sum(number_sum)
+
+print(f"The sum of the numbers is {number_sum} and the mean is {number_sum/4}")
