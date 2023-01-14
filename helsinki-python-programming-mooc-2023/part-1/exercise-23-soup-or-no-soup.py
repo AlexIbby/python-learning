036d4c42ee06ad394671bee3a43f0482bb4029be
AlexIbby <alex.a.ibrahim@gmail.com>
2023-01-14 14:25:15 -0500

    completed soup or no soup

---
+++ helsinki-python-programming-mooc-2023/part-1/exercise-23-soup-or-no-soup.py
@@ -0,0 +1,11 @@
+# Write your solution here
+
+name = str(input("Please tell me your name: "))
+
+if name == "Jerry":
+    print("Next please!")
+
+else:
+    portions = int(input("How many portions of soup?"))
+    print(f"The total cost is {5.90 * portions}")
+    print("Next please!")
