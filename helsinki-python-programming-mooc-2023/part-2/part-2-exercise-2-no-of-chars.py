66faec6353b4d6c42ce9e21fa49b81aee89afbad
AlexIbby <alex.a.ibrahim@gmail.com>
2023-01-18 20:51:43 -0500

    completed number of characters

---
+++ helsinki-python-programming-mooc-2023/part-2/part-2-exercise-2-no-of-chars.py
@@ -0,0 +1,11 @@
+# Write your solution here
+
+# get word and length 
+word = str(input("Please type in a word: "))
+length = len(word)
+
+if length > 1:
+    print(f"There are {length} letters in the word {word}")
+    print("Thank you!")
+else:
+    print("Thank you!")
