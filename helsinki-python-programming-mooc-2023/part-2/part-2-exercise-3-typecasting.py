b730be118f7056531824734310867aa14cfb1ceb
AlexIbby <alex.a.ibrahim@gmail.com>
2023-01-19 20:59:30 -0500

    completed typecasting exercise

---
+++ helsinki-python-programming-mooc-2023/part-2/part-2-exercise-3-typecasting.py
@@ -0,0 +1,9 @@
+# Write your solution here
+
+float_number = float(input("Please type in a number: "))
+
+integer_part = int(float_number)
+decimal_part = float_number - integer_part
+
+print(f"Integer part: {integer_part}")
+print(f"Decimal part: {decimal_part}")
