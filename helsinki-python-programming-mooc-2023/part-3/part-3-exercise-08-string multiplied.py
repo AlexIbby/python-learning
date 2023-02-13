17ab3d6779d1a06629cc632fad25c04b3dde2206
AlexIbby <alex.a.ibrahim@gmail.com>
2023-02-13 12:34:33 -0500

    string multiplied exercise

---
+++ helsinki-python-programming-mooc-2023/part-3/part-3-exercise-08-string multiplied.py
@@ -0,0 +1,9 @@
+# Write your code here
+
+# Write your solution here
+
+user_string = str(input("Please type in a string: "))
+
+amount = int(input("Please type in an amount: "))
+
+print(user_string*amount)
