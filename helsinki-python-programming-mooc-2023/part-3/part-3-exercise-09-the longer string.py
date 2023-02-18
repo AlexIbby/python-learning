91f73c16b64c2f3efc180761e50a205acf980a10
AlexIbby <alex.a.ibrahim@gmail.com>
2023-02-18 13:03:22 -0500

    completed a rectangle of hashes

--- helsinki-python-programming-mooc-2023/part-3/part-3-exercise-09-the longer string.py
+++ helsinki-python-programming-mooc-2023/part-3/part-3-exercise-09-the longer string.py
@@ -1,1 +1,18 @@
 # Write your code here
+
+# Write your solution here
+
+string_1 = str(input("Please type in string 1: "))
+
+string_2 = str(input("Please type in string 2: "))
+
+#check lengths
+
+if len(string_1) > len(string_2):
+    print(f"{string_1} is longer")
+
+elif len(string_2) > len(string_1):
+    print(f"{string_2} is longer")
+
+else:
+    print("The strings are equally long")
--- helsinki-python-programming-mooc-2023/part-3/part-3-exercise-12-a line of hashes.py
+++ helsinki-python-programming-mooc-2023/part-3/part-3-exercise-12-a line of hashes.py
@@ -1,7 +1,4 @@
 # Write your code here
 
-# Write your solution here
-
 width = int(input("Width"))
-
 print(width*"#")
