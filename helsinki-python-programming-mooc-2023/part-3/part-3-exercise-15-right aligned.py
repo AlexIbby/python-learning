06ff23ec77089d9d792742adef93c057af3a21a6
AlexIbby <alex.a.ibrahim@gmail.com>
2023-02-20 13:15:23 -0500

    completed right aligned exercise

---
+++ helsinki-python-programming-mooc-2023/part-3/part-3-exercise-15-right aligned.py
@@ -0,0 +1,10 @@
+# Write your code here
+
+# Write your solution here
+
+no_of_asteriks = 20
+
+user_string = str(input("Please type in a string: "))
+no_of_asteriks -= len(user_string)
+
+print("*"*no_of_asteriks + user_string)
