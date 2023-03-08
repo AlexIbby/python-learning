4c6028052556346834444f6ebfb9e36cfd5c21ac
AlexIbby <alex.a.ibrahim@gmail.com>
2023-03-07 19:11:18 -0500

    completed mean exercise

---
+++ helsinki-python-programming-mooc-2023/part-3/part-3-exercise-30-mean.py
@@ -0,0 +1,9 @@
+# Write your code here
+
+def mean(number1:int, number2:int, number3:int):
+    mean = (number1 + number2 + number3) / 3
+    print(mean)
+
+# Testing the function
+if __name__ == "__main__":
+    mean(1, 2, 3)
