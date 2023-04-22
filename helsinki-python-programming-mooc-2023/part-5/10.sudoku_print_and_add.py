3a8436ffe16b4c1c2a7842185822b19e3213cbf6
AlexIbby <alex.a.ibrahim@gmail.com>
2023-04-22 09:50:07 -0400

    completed sudoku print add, add to copy and remove smallest exercise

---
+++ helsinki-python-programming-mooc-2023/part-5/10.sudoku_print_and_add.py
@@ -0,0 +1,35 @@
+# Write your solution here
+
+def print_sudoku(sudoku:list):
+    modified_sudoku = []  # create a new list to hold the modified rows
+    for i, row in enumerate(sudoku):
+        modified_row = []  # create a new list to hold the modified items in the row
+        for j, item in enumerate(range(len(row))):
+            if sudoku[i][j] == 0:
+                modified_row.append("_")  # append the modified item to the modified row
+            else:
+                modified_row.append(str(sudoku[i][j]))  # convert non-zero items to string and append
+        modified_sudoku.append(modified_row)  # append the modified row to the modified sudoku
+    
+    # print formatting
+    for i, row in enumerate(modified_sudoku):
+        for j, item in enumerate(range(len(row))):
+            if j == 3:
+                modified_sudoku[i].insert(3, "")
+            if j == 7:
+                modified_sudoku[i].insert(7, "")
+                
+    # printing 
+    for i, row in enumerate(modified_sudoku):
+        row = " ".join(row)
+        if i == 3:
+            print()
+            
+        elif i == 6:
+            print()
+        print(row)
+
+#add number function
+def add_number(sudoku: list, row_no: int, column_no: int, number:int):
+    sudoku[row_no][column_no] = number
+    return sudoku
---
+++ helsinki-python-programming-mooc-2023/part-5/9.remove_smallest.py
@@ -0,0 +1,5 @@
+# Write your solution here
+
+def remove_smallest(numbers: list):
+    smallest = min(numbers)
+    numbers = numbers.remove(smallest)
