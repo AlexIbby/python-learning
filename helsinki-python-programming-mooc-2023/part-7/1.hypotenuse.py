862dc68a036b288eb3b1d0c8a3ec4da36ca99593
AlexIbby <alex.a.ibrahim@gmail.com>
2023-06-09 18:06:52 -0400

    Completed hypotenuse exercise in section 7

---
+++ helsinki-python-programming-mooc-2023/part-7/1.hypotenuse.py
@@ -0,0 +1,13 @@
+# Write your solution here
+
+from math import sqrt
+
+def  hypotenuse(leg1: float, leg2: float):
+    hypotenuse = (leg1**2) + (leg2**2)
+
+    hypotenuse= sqrt(hypotenuse)
+
+    return hypotenuse
+
+
+
